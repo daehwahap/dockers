2f1b63323c9a88fad5275d7218fa9edd8188bb64
11t518s <11t518s20161759@gmail.com>
2024-04-07 02:00:48 +0900

    fix: nginx 쉽지않음..

--- mysql/README.md
+++
@@ -1,8 +0,0 @@
-뭔가 더 쉽게하는 방법이 있을 것 같긴한데 찾지는 못해서 우선
-
-이중화 DB는
-https://velog.io/@zvyg1023/mysql-master-slave
-
-요 블로그 참조하여 하는걸로!
-
-docker-compose.yml이랑 각 Dockerfile, my.conf 만 작성 완료
---
+++ mysql/singleDB/init.sql
@@ -0,0 +1,17 @@
+USE mysql;
+
+-- -- localhost 에서만 접속 가능한 계정 생성
+-- CREATE USER 'mysql'@'localhost' IDENTIFIED BY 'mysql';
+-- GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'localhost';
+-- FLUSH PRIVILEGES;
+
+-- 모든 DB, 테이블에 접속 가능한 계정 생성
+CREATE USER IF NOT EXISTS 'mysql'@'%' IDENTIFIED BY 'mysql';
+GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%';
+FLUSH PRIVILEGES;
+
+
+-- -- 추가 계정 생성 (예: sa)
+-- CREATE USER 'sa'@'%' IDENTIFIED BY 'password';
+-- GRANT ALL PRIVILEGES ON *.* TO 'sa'@'%';
+-- FLUSH PRIVILEGES;
